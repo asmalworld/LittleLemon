8f0a7f3edbae0695b5bc6e86c72e48734a807b2c
asmalworld <asmalworld@hotmail.com>
2024-02-14 15:44:35 -0500

    update

--- LittleLemon/littlelemon/littlelemon/settings.py
+++ LittleLemon/littlelemon/littlelemon/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'restaurant',
 ]
 
 MIDDLEWARE = [
@@ -51,10 +52,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'littlelemon.urls'
 
+#Include 'templates' in 'DIRS' attribute
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'BACKEND': 'django.template.backends.django.DjangoTemplates'
+        'DIRS': ['templates']
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
